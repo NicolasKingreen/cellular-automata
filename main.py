1bb3651bd24bc3915a9bb5e3d7afac26557b91fe
Nicolas Kingreen <NicolasKingreen@gmail.com>
2022-12-13 15:01:27 +0300

    -smooth drawing

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import sys
 from grid import Grid
 
 
-WIN_SIZE = 640, 420
-TARGET_FPS = 60
+WIN_SIZE = 640, 480
+TARGET_FPS = 0
 
-CELL_SIZE = 10
+CELL_SIZE = 20
 
-GEN_TIME_MS = 500  # 17 ms for 60 fps
+GEN_TIME_MS = 500
 SPEED_MULTIPLIERS = [1, 2, 4, 8, 16]
 
 
@@ -25,14 +25,18 @@ class Application:
         self.grid = Grid(*WIN_SIZE, CELL_SIZE)
         self.paused = True
         self.current_gen_time_ms = 0
-        self.current_speed_index = 0
+        self.current_speed_index = 2
+
+        self.lmb_pressed = False
 
     def run(self):
         while True:
 
             frame_time_ms = self.clock.tick(TARGET_FPS)
 
-            new_caption = f'Cellular Automata (FPS: {self.clock.get_fps():.2f})'
+            new_caption = f'Cellular Automata ' \
+                          f'(FPS: {self.clock.get_fps():.2f}) ' \
+                          f'({SPEED_MULTIPLIERS[self.current_speed_index]}x speed)'
             if self.paused:
                 new_caption += ' (paused)'
             pygame.display.set_caption(new_caption)
@@ -53,9 +57,16 @@ class Application:
                         self.grid.clear()
                 elif event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        self.grid.add_cell(*event.pos)
+                        self.lmb_pressed = True
+                        # self.grid.add_cell(*event.pos)
                     elif event.button == 3:
                         self.grid.remove_cell(*event.pos)
+                elif event.type == MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.lmb_pressed = False
+
+            if self.lmb_pressed:
+                self.grid.add_cell(*pygame.mouse.get_pos())
 
             if not self.paused:
                 self.current_gen_time_ms += frame_time_ms
